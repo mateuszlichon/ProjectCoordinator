7ada688091bb3a1be83108e768e3cb5b0a1c9114
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-08 18:57:00 +0200

    adds login controller

---
+++ src/main/java/com/lichon/projectcoordinator/web/LoginController.java
@@ -0,0 +1,17 @@
+package com.lichon.projectcoordinator.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String loginPage(ModelMap map) {
+		// prefix: /WEB-INF/view
+		// postfix: .jsp
+		return "login";
+	}
+}
