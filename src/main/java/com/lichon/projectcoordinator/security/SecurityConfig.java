6014b2126174249f38abf5e6b4eefd837fbf4157
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-08 18:28:30 +0200

    permits login access

--- src/main/java/com/lichon/projectcoordinator/security/SecurityConfig.java
+++ src/main/java/com/lichon/projectcoordinator/security/SecurityConfig.java
@@ -20,7 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated()
+		http.authorizeRequests()
+		.antMatchers("/login").permitAll()
+		.anyRequest().authenticated()
 		.and().formLogin()
 		.loginPage("/login")
 		.and().httpBasic();
