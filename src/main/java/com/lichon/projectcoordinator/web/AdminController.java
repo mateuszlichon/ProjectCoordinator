d3ba5603987a45353aced818a9459404fcf02f58
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-09 13:04:24 +0200

    adds principle to admin controller

--- src/main/java/com/lichon/projectcoordinator/web/AdminController.java
+++ src/main/java/com/lichon/projectcoordinator/web/AdminController.java
@@ -1,5 +1,7 @@
 package com.lichon.projectcoordinator.web;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AdminController {
 
 	@RequestMapping(value = "admin", method = RequestMethod.GET)
-	public String adminPage(ModelMap model) {
+	public String adminPage(ModelMap model, Principal principal) {
+		System.out.println(principal.getName());
 		return "admin";
 	}
 }
