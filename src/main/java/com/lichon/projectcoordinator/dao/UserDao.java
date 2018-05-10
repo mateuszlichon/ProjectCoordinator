f8b42aea25ee2afb443b1dee3f68ffdff2d5a369
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-10 17:37:51 +0200

    develops dao

--- src/main/java/com/lichon/projectcoordinator/dao/UserDao.java
+++ src/main/java/com/lichon/projectcoordinator/dao/UserDao.java
@@ -1,35 +1,64 @@
 package com.lichon.projectcoordinator.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.sql.DataSource;
+
+import com.lichon.projectcoordinator.domain.Authorities;
+import com.lichon.projectcoordinator.domain.Users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
-import com.lichon.projectcoordinator.domain.Users;
-
 @Repository
 public class UserDao implements UserDetailsService {
 
-	@Autowired
-	private DataSource dataSource;
-	private JdbcTemplate template;
-	
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		String sql = "select * from users where username = ?";
-		Users users = getJdbcTemplate().queryForObject(sql, new Object[] {username}, Users.class);
-		return users;
-	}
-	
-	private JdbcTemplate getJdbcTemplate() {
-		if (template == null) {
-			return new JdbcTemplate(dataSource);
-		} else {
-			return this.template;
+	  @Autowired
+	  private DataSource dataSource;
+	  private JdbcTemplate jdbcTemplate;
+	  
+		public UserDetails loadUserByUsername(String username)
+	      throws UsernameNotFoundException
+	  {
+			String sql = "select * from users where username = ?";
+			Users user = (Users)getJdbcTemplate().query(sql, new Object[] { username }, new RowMapper<Users>()
+			{
+				public Users mapRow(ResultSet rs, int rowNum) throws SQLException
+	      {
+					Users user = new Users();
+					if (user.getUsername() == null)
+					 user.setUsername(rs.getString("username"));
+					if (user.getPassword() == null)
+					  user.setPassword(rs.getString("password"));
+					return user;
+	      }
+			});
+			
+			sql = "select authority from authorities where username = ?";
+			List<String> authorities = getJdbcTemplate().queryForList(sql, new Object[] {username}, String.class);
+			Set<Authorities> userAuths = new HashSet<Authorities>();
+			for (String authority : authorities)
+			{
+				userAuths.add(new Authorities(username, authority));
+			}
+			user.setAuthorities(userAuths);
+			return user;
+	  }
+
+		private JdbcTemplate getJdbcTemplate ()
+		{
+			if (this.jdbcTemplate == null)
+				return new JdbcTemplate(dataSource);
+			else 
+				return jdbcTemplate;
 		}
 	}
-
-}
--- src/main/java/com/lichon/projectcoordinator/domain/Authorities.java
+++ src/main/java/com/lichon/projectcoordinator/domain/Authorities.java
@@ -8,12 +8,27 @@ public class Authorities implements GrantedAuthority {
 	private String username;
 	private String authority;
 
-	public String getAuthority() {
-		return this.authority;
-	}
-
-	public String getUsername() {
-		return username;
-	}
+	  public Authorities (String username, String authority)
+	  {
+	  	this.username = username;
+	  	this.authority = authority;
+	  }
+	  
+		public String getUsername()
+		{
+			return username;
+		}
+		public void setUsername(String username)
+		{
+			this.username = username;
+		}
+		public String getAuthority()
+		{
+			return authority;
+		}
+		public void setAuthority(String authority)
+		{
+			this.authority = authority;
+		}
 
 }
