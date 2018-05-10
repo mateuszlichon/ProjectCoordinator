9d871713aa873d2bf241e63456ae6c3d995a05b3
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-10 16:31:39 +0200

    adds authorities for grantedauthority

---
+++ src/main/java/com/lichon/projectcoordinator/domain/Authorities.java
@@ -0,0 +1,12 @@
+package com.lichon.projectcoordinator.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authorities implements GrantedAuthority {
+
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
