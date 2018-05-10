226f807023cef3ba720ee37ad387b96417e5980f
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-10 16:29:59 +0200

    adds users class for userdetails

---
+++ src/main/java/com/lichon/projectcoordinator/domain/Users.java
@@ -0,0 +1,49 @@
+package com.lichon.projectcoordinator.domain;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class Users implements UserDetails {
+
+	private String username;
+	private String password;
+	
+	
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
